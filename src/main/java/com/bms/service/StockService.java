a077ace44fce73cf0c1fab3b1e49efd2b33905ae
padarabinda panigrahi <padarabinda.panigrahi@cognizant.com>
2017-05-26 12:11:58 +0530

    service layer

---
+++ src/main/java/com/bms/service/StockService.java
@@ -0,0 +1,21 @@
+package com.bms.service;
+
+import org.json.simple.JSONObject;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StockService {
+	
+	public JSONObject getList() {
+		return null;
+	}
+	
+	public JSONObject insertList(String input) {
+		return null;
+	}
+	
+	public JSONObject updateList(String id) {
+		return null;
+	}
+	
+}
