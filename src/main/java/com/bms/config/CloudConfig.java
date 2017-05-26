7bbc9eb187a0418ae9d013b346cc7af08914002d
pulkit <pulkit.bajpai@cognizant.com>
2017-05-26 14:52:27 +0530

    mongo configuration

--- src/main/java/com/bms/config/CloudConfig.java
+++ src/main/java/com/bms/config/CloudConfig.java
@@ -1,15 +1,39 @@
 package com.bms.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.MongoDbFactory;
 
+import com.bms.utils.Utils;
+
+import com.google.gson.JsonObject;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoDatabase;
+
 @Configuration
+
 public class CloudConfig extends AbstractCloudConfig {
-	
+	private MongoClient mongoClient;
+	@Autowired
+	private Utils utils;
+
 	@Bean
 	public MongoDbFactory documentMongoDbFactory() {
 		return connectionFactory().mongoDbFactory();
+	}
+
+	@Bean
+	public MongoDatabase mongoDatabase() {
+
+		String uri = null;
+		JsonObject cloudConfig = utils
+				.getCredentials(System.getenv(utils.getMongoVcapServiceName(System.getenv("VCAP_SERVICES"))));
+		uri = cloudConfig.get("uri").getAsString();
+		mongoClient = new MongoClient(new MongoClientURI(uri));
+		String databaseName = uri.substring(uri.lastIndexOf("/") + 1, uri.length());
+		return mongoClient.getDatabase(databaseName);
 	}
 }
--- src/main/java/com/bms/utils/Utils.java
+++ src/main/java/com/bms/utils/Utils.java
@@ -47,7 +47,27 @@ public class Utils {
 		return serviceName;
 
 	}
+	public String getMongoVcapServiceName(String vcap_service) {
+		JSONParser jsonParser = new JSONParser();
+		JSONObject bluePrintObject;
+		String serviceName = null;
+		try {
+			bluePrintObject = (JSONObject) jsonParser.parse(vcap_service);
+			for (Object key : bluePrintObject.keySet()) {
+				if (key.toString().toLowerCase().contains("mongo")) {
+					JSONArray obj = (JSONArray) bluePrintObject.get(key.toString());
+					JSONObject object = (JSONObject) obj.get(0);
+					serviceName = (String) object.get("name");
+				}
 
+			}
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return serviceName;
+
+	}
 	/** Method to get the app name from environment variables
 	 * 
 	 * @param vcap_app
