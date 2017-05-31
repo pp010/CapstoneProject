fb844a131a970724a79c8f0b4f9166cb420a56c6
540497 <540497@PC276237.cts.com>
2017-05-31 14:41:16 +0530

    testing R2 commit

--- src/main/java/com/bms/BmsApplication.java
+++ src/main/java/com/bms/BmsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BmsApplication {
-
+//testing R2 commit
 	public static void main(String[] args) {
 		SpringApplication.run(BmsApplication.class, args);
 	}
