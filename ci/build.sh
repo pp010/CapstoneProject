8d311948f6f16d10e4699c0f291940c317c93c98
pp010 <padarabinda.panigrahi@cognizant.com>
2017-06-30 22:07:23 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -2,13 +2,6 @@
 #set -e -x
 git clone ppTest resource-app
 echo "cloning done"
-
-for entry in *
-        do echo $entry
-        done
-        
-        echo "end of loop"
-        
 cd resource-app
 ls
 cd ppTest
