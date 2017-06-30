3ea83b8a7a43d34660793a0bf878e6f2568ede0b
pp010 <padarabinda.panigrahi@cognizant.com>
2017-06-30 17:08:58 +0530

    commit

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #set -e -x
 git clone ppTest resource-app
+echo "cloning done"
 cd resource-app/CapstoneProject
+echo "inside resource-app/CapstoneProject"
 #mvn install
 #mvn install -DskipTests=false
 mvn clean install -e -DskipTests=true || ErrorHandler "Build Failed for test"
