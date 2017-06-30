4c86cdc88d51088253fcb17beb64badd0d18e11b
pp010 <padarabinda.panigrahi@cognizant.com>
2017-06-30 16:57:44 +0530

    commit

--- ci/build.sh
+++ ci/build.sh
@@ -1,18 +1,10 @@
 #!/usr/bin/env bash
-#set -e -x
-#DIR="/tmp/build/"
-#rm -r -- "$DIR"*
-#for entry in `ls $tmp`; do
-   # echo $entry
-#done
-#for entry in `ls $build`; do
-   # echo $entry
-#done
+set -e -x
 git clone ppTest resource-app
 cd resource-app/CapstoneProject
 #mvn install
-mvn install -DskipTests=true
+mvn install -DskipTests=false
 echo "------installation done---------"
-
 #mvn test
 echo "------testing done------"
+cd ..
