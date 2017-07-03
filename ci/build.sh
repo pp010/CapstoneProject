cbb13aa5bbd5cc44af0f2b8103c165c5adf2e75b
pp010 <padarabinda.panigrahi@cognizant.com>
2017-07-03 18:44:25 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -6,11 +6,5 @@ cd resource-app
 echo "About to print pwd2 value -"
 echo $(pwd)
 mvn clean install -e -DskipTests=true || ErrorHandler "Build Failed for test"
-cd ..
-cd ..
-mkdir /put/CapstoneProject/
-cd ./put/CapstoneProject/
-echo "About to print pwd3 value -"
-echo $(pwd)
 cp /root/.m2/repository/com/cognizant/BidManagementSystem/0.1/BidManagementSystem-0.1.jar ./
 echo "------installation done---------"
