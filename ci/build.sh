d0d5bf33f5c6d155afd517d9386e6521f8f40942
pp010 <padarabinda.panigrahi@cognizant.com>
2017-07-03 15:49:59 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -7,10 +7,13 @@ echo $(pwd)
 cd resource-app
 echo "About to print pwd2 value -"
 echo $(pwd)
-cd ppTest
-echo "About to print pwd3 value -"
-echo $(pwd)
-var = $(pwd) 
+
+var = $(pwd)
+for entry in "$var"/*
+do
+  echo "$entry"
+done
+ 
 #cd resource-app/CapstoneProject
 echo "inside resource-app/CapstoneProject"
 #mvn install
