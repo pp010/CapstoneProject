4a234ac98c932633671534d032884c803192ad92
pp010 <padarabinda.panigrahi@cognizant.com>
2017-07-03 16:21:43 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -5,6 +5,5 @@ echo $(pwd)
 cd resource-app
 echo "About to print pwd2 value -"
 echo $(pwd)
-echo "inside resource-app/CapstoneProject"
 mvn clean install -e -DskipTests=true || ErrorHandler "Build Failed for test"
 echo "------installation done---------"
