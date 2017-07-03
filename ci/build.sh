480157f3b9ecae125741c6a39ab4cccacd3c1303
pp010 <padarabinda.panigrahi@cognizant.com>
2017-07-03 17:30:44 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -6,4 +6,5 @@ cd resource-app
 echo "About to print pwd2 value -"
 echo $(pwd)
 mvn clean install -e -DskipTests=true || ErrorHandler "Build Failed for test"
+cp /target/BidManagementSystem-0.1.jar ../resource-app
 echo "------installation done---------"
