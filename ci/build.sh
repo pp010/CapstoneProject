bf3b31072a5da7133a8ab681797af65272fad4e4
pp010 <padarabinda.panigrahi@cognizant.com>
2017-07-03 18:30:18 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -12,5 +12,5 @@ echo $(pwd)
 cd ..
 echo "About to print pwd3 value -"
 echo $(pwd)
-cp /root/.m2/repository/com/cognizant/BidManagementSystem/0.1/BidManagementSystem-0.1.jar /build/
+cp /root/.m2/repository/com/cognizant/BidManagementSystem/0.1/BidManagementSystem-0.1.jar /AppDeploy/
 echo "------installation done---------"
