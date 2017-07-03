5e37a54c023ec388c416965a4d5015d61dffbb31
pp010 <padarabinda.panigrahi@cognizant.com>
2017-07-03 13:52:46 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+#set -e -x
 git clone ppTest resource-app
 echo "cloning done"
+cd resource-app
+ls
 cd ppTest
+pwd
+#cd resource-app/CapstoneProject
 echo "inside resource-app/CapstoneProject"
+#mvn install
+#mvn install -DskipTests=false
 mvn clean install -e -DskipTests=true || ErrorHandler "Build Failed for test"
 mv CapstoneProject/build/libs/*.jar ../resource-app
 echo "------installation done---------"
+#mvn test
