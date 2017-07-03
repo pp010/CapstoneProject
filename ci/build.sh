4fd6e24a2d0649b2b3567c10359556071315e4c8
pp010 <padarabinda.panigrahi@cognizant.com>
2017-07-03 15:44:03 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -2,10 +2,13 @@
 #set -e -x
 git clone ppTest resource-app
 echo "cloning done"
+echo "About to print pwd1 value -"
+echo $(pwd)
 cd resource-app
-ls
+echo "About to print pwd2 value -"
+echo $(pwd)
 cd ppTest
-echo "About to print pwd value -"
+echo "About to print pwd3 value -"
 echo $(pwd)
 var = $(pwd) 
 #cd resource-app/CapstoneProject
@@ -14,7 +17,7 @@ echo "inside resource-app/CapstoneProject"
 #mvn install -DskipTests=false
 mvn clean install -e -DskipTests=true || ErrorHandler "Build Failed for test"
 cp $var/target/*.jar /home/ubuntu/
-cp $(var)/target/*.jar /home/ubuntu/
+cp $(pwd)/target/*.jar /home/ubuntu/
 #mv ppTest/build/libs/*.jar ../resource-app
 echo "------installation done---------"
 #mvn test
