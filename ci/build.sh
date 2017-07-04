052ff70607937759ea1892cdc6d54a10d0600ca6
pp010 <padarabinda.panigrahi@cognizant.com>
2017-07-04 19:28:19 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -7,7 +7,7 @@ echo $(pwd)
 cd resource-app
 echo "About to print pwd2 value -"
 echo $(pwd)
-#mvn clean install -e -DskipTests=true || ErrorHandler "Build Failed for test"
+mvn clean install -e -DskipTests=true || ErrorHandler "Build Failed for test"
 echo $(ls -lart)
 cd /tmp/build/3bb15873/resource-app/target
 echo $(ls)
