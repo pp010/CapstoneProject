00b19699e58d58aeed52c8484c33d01f1bb1607a
pp010 <padarabinda.panigrahi@cognizant.com>
2017-07-04 16:59:52 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-
-pushd CapstoneProject
-  ./mvnw clean package
-popd
-
+git clone CapstoneProject resource-app
+echo "cloning done"
+echo "About to print pwd1 value -"
+echo $(pwd)
+cd resource-app
+echo "About to print pwd2 value -"
+echo $(pwd)
+mvn clean install -e -DskipTests=true || ErrorHandler "Build Failed for test"
+echo "------installation done---------"
 cp CapstoneProject/target/BidManagementSystem-0.1.jar  resource-app/.
