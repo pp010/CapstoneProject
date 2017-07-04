5eb822375c0b010894f40130af704da3b95ceeeb
pp010 <padarabinda.panigrahi@cognizant.com>
2017-07-04 19:37:30 +0530

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
-
+sudo su
 git clone CapstoneProject resource-app
 echo "cloning done"
 echo "About to print pwd1 value -"
 echo $(pwd)
+chmod 777 resource-app
 cd resource-app
 echo "About to print pwd2 value -"
 echo $(pwd)
 mvn clean install -e -DskipTests=true || ErrorHandler "Build Failed for test"
 echo $(ls -lart)
-cd /tmp/build/3bb15873/resource-app/target
+cd target
 echo $(ls)
 echo "------installation done---------"
 echo $(pwd)
