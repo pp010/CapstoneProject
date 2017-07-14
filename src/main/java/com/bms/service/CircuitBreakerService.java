d5ed2c386c619a075495289fdbd3c7f8dffd484d
pp010 <padarabinda.panigrahi@cognizant.com>
2017-07-14 15:57:12 +0530

    hystrix properties updated

--- src/main/java/com/bms/service/CircuitBreakerService.java
+++ src/main/java/com/bms/service/CircuitBreakerService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 @Service
 public class CircuitBreakerService {
@@ -13,9 +12,7 @@ public class CircuitBreakerService {
 	@Autowired
 	RestTemplate restTemplate;
 
-	@HystrixCommand(fallbackMethod = "getBackupGuide", commandProperties = {
-			@HystrixProperty(name = "circuitBreaker.requestVolumeThreshold", value = "5"),
-			@HystrixProperty(name = "circuitBreaker.sleepWindowInMilliseconds", value = "10") })
+	@HystrixCommand(fallbackMethod = "getBackupGuide", commandKey = "hystrixKey")
 	public String getGuide() {
 		return "<inside getGuid>";
 	}
